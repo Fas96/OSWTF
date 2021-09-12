5c087c9314681ae53fb844e9526cbe65f730cf1f
Fas96 <firibu.anyass@gmail.com>
2021-09-12 23:05:16 +0900

    migratory birds

--- qbl-5.py
+++ qbl-5.py
@@ -28,9 +28,37 @@ def divisibleSumPairs(n, k, ar):
             if (ar[i] + ar[j]) % k == 0:
                 mak.append([ar[j], ar[i]])
 
-    print(mak)
+    # print(mak)
+
+
+def migratoryBirds(arr):
+    arr = [1, 1, 4, 4, 5, 3]
+    arr = sorted(arr)
+    dArr = {}
+
+    for i in sorted(arr):
+        if i not in dArr:
+            dArr[i] = 1
+        else:
+            dArr[i] += 1
+
+    sorted_tuples = sorted(dArr.items(), key=lambda item: item[1])
+
+    sorted_dict = {k: v for k, v in sorted_tuples}
+
+    ls = sorted_dict.popitem()
+    bls = sorted_dict.popitem()
+
+    res = 0
+    if ls[1] == bls[1]:
+        res = bls[0]
+    else:
+        res = ls[0]
+
+    return res
 
 
 if __name__ == '__main__':
+    migratoryBirds([])
     divisibleSumPairs(2, 3, [])
     oddNumbers(3, 9)
